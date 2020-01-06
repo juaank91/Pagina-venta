67b2ba016479eacd855e2bdc37685d74466b5d40
juaank91 <58111338+juaank91@users.noreply.github.com>
2020-01-05 23:15:38 -0300

    Se agrega mensajes de confirmacion de add y del de productos

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ class UI{
                     <div class="col-md-2 py-1 pr-1 ml-auto my-1 ">
                         <a href="#" class="btn btn-success btn-sm d-block">Guardar</a>    
                         <a href="#" class="btn btn-primary btn-sm d-block">Modificar</a>
-                        <a class="btn btn-danger btn-sm d-block text-white" name="del">Eliminar</a>
+                        <a class="btn btn-danger btn-sm d-block" id="delete">Eliminar</a>
                     </div>
                 </div>
             </div>`;
@@ -48,10 +48,23 @@ class UI{
     
     //Metodo para eliminar un producto de la lista.
     eliminarProducto(element){
-        if(element.name === "del"){
-            element.parentElement.parentElement.parentElement.remove();
+        if(element.id === "delete"){
+            var opcion = confirm("Estas seguro que deseas elminar este producto");
+            if(opcion == true){
+                element.parentElement.parentElement.parentElement.remove();
+                this.mostrarMensaje("Elemento eliminado satisfactorimente", "danger");
+            }
         }
     }
+
+    modificarProducto(producto){
+
+    }
+
+    validacionForm(producto){
+       
+    }
+
 
     //Metodo para mostrar mensaje de confirmacion apenas se realize una accion.
     mostrarMensaje(mensaje, cssClass){
@@ -83,24 +96,48 @@ document.getElementById('formulario-add')
         const producto = new Producto(codigo, nombre , precio);
         const ui = new UI();
         e.preventDefault();
-        if(codigo === '' || nombre === '' || precio === ''){
+        ui.validacionForm(producto);
+        /*if(codigo === '' || nombre === '' || precio === ''){
             return ui.mostrarMensaje("Favor completa todos los campos antes de continuar", 'danger')
         }
+        */
+        //se añade producto a la lista para desplegar los productos ingresados. 
         ui.addProducto(producto);
-        
+        //Resetea el formulario con el metodo reseteoform() de la clase ui.
         ui.reseteoform();
+        //llama al metodo mostrar mensaje de la clases ui para mostrar mensaje en pantalla.
         ui.mostrarMensaje("Se a añadido el producto correctamente", 'success');
-        
+        ui.validacionForm(producto);
 })
 
 //Creacion de una funcion para eliminar un producto de la lista apenas se haga click en el boton eliminar
-document.getElementById("lista-productos").addEventListener('click', function(e){
-    const ui=new UI();
-    var opcion = confirm("¿Estas eliminando un producto estas seguro de querer continuar?")
-    if(opcion== true){
-        ui.eliminarProducto(e.target);    
-        ui.mostrarMensaje("El producto a sido eliminado satisfactoriamente", 'danger')  
+document.getElementById("lista-productos")
+    .addEventListener('click', function(e){
+        const ui=new UI();
+        //llama al metodo eliminar de la clase ui y verifica el evento con e.target para asignar el elemento a eliminar.
+        ui.eliminarProducto(e.target);
+        //llama al metodo modificar de la clase ui.
+        ui.modificarProducto();
+    
+})
+var codi = document.getElementById("codigo"). 
+    addEventListener('blur', ()=>{
+        var cod = document.getElementById("codigo").value;
+        console.log(cod);
+        if(cod == "null" || cod == "" ){
+            console.log(cod);
+            const span= document.createElement('span');
+            span.id = "error_codigo";
+            span.className="alert alert-danger d-block mb-1";
+            span.append(document.createTextNode("Debe ingresar un codigo correcto"))
+
+            console.log(span);
+
+            const tarjeta = document.getElementById("tarjeta");
+            const formulario = document.getElementById("formulario-add");
+            tarjeta.insertBefore(span,formulario);
+            setTimeout(() =>{
+                document.getElementById("error_codigo").remove();
+            },2000)
     }
 })
-
-
